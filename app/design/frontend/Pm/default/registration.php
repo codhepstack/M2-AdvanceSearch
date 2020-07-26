a0a853b47fecfaaaf73cc7eb0ea67553689fb78e
Jens Müller <jens.mueller@pixelmechanics.de>
2020-07-26 17:25:38 +0530

    Removed the unnecessary files

--- app/design/frontend/Pm/default/registration.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Magento\Framework\Component\ComponentRegistrar;
-
-ComponentRegistrar::register(
-    ComponentRegistrar::THEME, 'frontend/Pm/default', __DIR__
-);
--- app/design/frontend/Pm/default/requirejs-config.js
+++
@@ -1,7 +0,0 @@
-var config = {
-    shim: {
-        'fotorama/fotorama': {
-            deps: ['jquery']
-        }
-    }
-};
